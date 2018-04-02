6e54597a16f824228597fafe42dd28fc394a4fff
byunhyeongwon <bhw0506@gmail.com>
2018-04-02 16:23:48 +0900

    authentication

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,39 @@
 //Route::get('/', 'WelcomeController@index');
 
 Route::resource('articles', 'ArticlesController');
+
+Route::get('auth/login', function() {
+
+    $credentials = [
+        'email' => 'josh@gmail.com',
+        'password' => 'password'
+    ];
+
+    if(! auth()->attempt($credentials)) {
+
+        return '로그인 정보가 정확하지 않습니다.';
+    }
+
+    return redirect('protected');
+});
+
+Route::get('protected', function() {
+
+    dump(session()->all());
+
+    if(! auth()->check()) {
+
+        return 'who are you?';
+    }
+
+    return 'welcome'. auth()->user()->name;
+});
+
+Route::get('auth/logout', function() {
+    auth() -> logout();
+
+    return 'see you~';
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
