ee4ebf8a9473bc536afa6f3cccd96e1ff8306982
byunhyeongwon <bhw0506@gmail.com>
2018-04-03 10:27:05 +0900

    validation

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -27,7 +27,8 @@ class ArticlesController extends Controller
      */
     public function create()
     {
-        return __METHOD__. '은(는) Article 컬렉션을 만들기 위한 폼을 담은 뷰를 반환합니다.';
+        //return __METHOD__. '은(는) Article 컬렉션을 만들기 위한 폼을 담은 뷰를 반환합니다.';
+        return view('articles.create');
     }
 
     /**
@@ -38,7 +39,43 @@ class ArticlesController extends Controller
      */
     public function store(Request $request)
     {
-        return __METHOD__. '은(는) 사용자의 입력한 폼 데이터로 새로운 Article 컬렉션을 만듭니다.';
+        //return __METHOD__. '은(는) 사용자의 입력한 폼 데이터로 새로운 Article 컬렉션을 만듭니다.';
+
+       /* $rules = [
+
+            'title' => ['required'],
+            'content' => ['required', 'min:10'],
+        ];
+
+        $validator = \Validator::make($request->all(), $rules);
+
+        if($validator->fails()) {
+
+            return back()->withErrors($validator)
+                ->withInput();
+        }
+
+        $article = \App\User::find(1)->articles()
+                                ->create($request->all());
+
+        if(! $article) {
+
+            return back()->with('flash_message', '글이 저장되지 않습니다.')
+                ->withInput();
+        }
+
+        return redirect(route('articles.index'))
+            ->with('flash_message', '작성하신 글이 저장되었습니다.');*/
+
+       $article = \App\User::find(1)->articles()->create($request->all());
+
+       if(! $article) {
+
+           return back() -> with('flash_message', '글이 저장되지 않았습니다.')
+               ->withInput();
+       }
+
+       return redirect(route('articles.index'))->with('flash_message', '작성하신 글이 저장 되었씁니다.');
     }
 
     /**
